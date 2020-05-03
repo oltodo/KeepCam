cb1decd44aeaf8dc58d4d27f4238eecaeed0d4e0
Nicolas Bazille <oltodo@msn.com>
2020-05-03 21:16:33 +0200

    Initial Commit

---
+++ KeepCam/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KeepCam
+//
+//  Created by BAZILLE NICOLAS on 03/05/2020.
+//  Copyright © 2020 BAZILLE NICOLAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
